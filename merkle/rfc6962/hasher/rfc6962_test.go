24add13b9a4d4a7c9672b77622adaf67166d99ed
Al Cutter <al@google.com>
2021-01-14 16:04:52 +0000

    separate rfc6962 tests

--- merkle/rfc6962/hasher/rfc6962_test.go
+++ merkle/rfc6962/hasher/rfc6962_test.go
@@ -11,22 +11,22 @@
 // WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 // See the License for the specific language governing permissions and
 // limitations under the License.
-package hasher
+package hasher_test
 
 import (
 	"bytes"
 	"encoding/hex"
-	"fmt"
 	"testing"
 
 	_ "github.com/golang/glog"
+	"github.com/google/trillian/merkle/rfc6962/hasher"
 )
 
 func TestRFC6962Hasher(t *testing.T) {
-	hasher := DefaultHasher
+	h := hasher.DefaultHasher
 
-	leafHash := hasher.HashLeaf([]byte("L123456"))
-	emptyLeafHash := hasher.HashLeaf([]byte{})
+	leafHash := h.HashLeaf([]byte("L123456"))
+	emptyLeafHash := h.HashLeaf([]byte{})
 
 	for _, tc := range []struct {
 		desc string
@@ -37,7 +37,7 @@ func TestRFC6962Hasher(t *testing.T) {
 		{
 			desc: "RFC6962 Empty",
 			want: "e3b0c44298fc1c149afbf4c8996fb92427ae41e4649b934ca495991b7852b855",
-			got:  hasher.EmptyRoot(),
+			got:  h.EmptyRoot(),
 		},
 		// Check that the empty hash is not the same as the hash of an empty leaf.
 		// echo -n 00 | xxd -r -p | sha256sum
@@ -56,7 +56,7 @@ func TestRFC6962Hasher(t *testing.T) {
 		{
 			desc: "RFC6962 Node",
 			want: "aa217fe888e47007fa15edab33c2b492a722cb106c64667fc2b044444de66bbb",
-			got:  hasher.HashChildren([]byte("N123"), []byte("N456")),
+			got:  h.HashChildren([]byte("N123"), []byte("N456")),
 		},
 	} {
 		t.Run(tc.desc, func(t *testing.T) {
@@ -73,59 +73,37 @@ func TestRFC6962Hasher(t *testing.T) {
 
 // TODO(pavelkalinnikov): Apply this test to all LogHasher implementations.
 func TestRFC6962HasherCollisions(t *testing.T) {
-	hasher := DefaultHasher
+	h := hasher.DefaultHasher
 
 	// Check that different leaves have different hashes.
 	leaf1, leaf2 := []byte("Hello"), []byte("World")
-	hash1 := hasher.HashLeaf(leaf1)
-	hash2 := hasher.HashLeaf(leaf2)
+	hash1 := h.HashLeaf(leaf1)
+	hash2 := h.HashLeaf(leaf2)
 	if bytes.Equal(hash1, hash2) {
 		t.Errorf("Leaf hashes should differ, but both are %x", hash1)
 	}
 
 	// Compute an intermediate subtree hash.
-	subHash1 := hasher.HashChildren(hash1, hash2)
+	subHash1 := h.HashChildren(hash1, hash2)
 	// Check that this is not the same as a leaf hash of their concatenation.
 	preimage := append(hash1, hash2...)
-	forgedHash := hasher.HashLeaf(preimage)
+	forgedHash := h.HashLeaf(preimage)
 	if bytes.Equal(subHash1, forgedHash) {
 		t.Errorf("Hasher is not second-preimage resistant")
 	}
 
 	// Swap the order of nodes and check that the hash is different.
-	subHash2 := hasher.HashChildren(hash2, hash1)
+	subHash2 := h.HashChildren(hash2, hash1)
 	if bytes.Equal(subHash1, subHash2) {
 		t.Errorf("Subtree hash does not depend on the order of leaves")
 	}
 }
 
-// TODO(al): Remove me.
-func BenchmarkHashChildrenOld(b *testing.B) {
-	h := DefaultHasher
-	l := h.HashLeaf([]byte("one"))
-	r := h.HashLeaf([]byte("or other"))
-	for i := 0; i < b.N; i++ {
-		_ = h.hashChildrenOld(l, r)
-	}
-}
-
 func BenchmarkHashChildren(b *testing.B) {
-	h := DefaultHasher
+	h := hasher.DefaultHasher
 	l := h.HashLeaf([]byte("one"))
 	r := h.HashLeaf([]byte("or other"))
 	for i := 0; i < b.N; i++ {
 		_ = h.HashChildren(l, r)
 	}
-}
-
-func TestHashChildrenEquivToOld(t *testing.T) {
-	h := DefaultHasher
-	for i := 0; i < 1000; i++ {
-		l := h.HashLeaf([]byte(fmt.Sprintf("leaf left %d", i)))
-		r := h.HashLeaf([]byte(fmt.Sprintf("leaf right %d", i)))
-		if oldHash, newHash := h.hashChildrenOld(l, r), h.HashChildren(l, r); !bytes.Equal(oldHash, newHash) {
-			t.Errorf("%d different hashes: %x vs %x", i, oldHash, newHash)
-		}
-	}
-
 }
